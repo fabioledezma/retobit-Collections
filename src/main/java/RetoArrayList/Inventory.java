7888a9dc5eb7ddaf1aa2105c16b23654cd9b01ed
Fabio Ledezma <fabioledezmadev17@gmail.com>
2026-04-21 17:34:27 -0500

    resolucion de clase Inventory

--- src/main/java/RetoArrayList/Inventory.java
+++ src/main/java/RetoArrayList/Inventory.java
@@ -6,21 +6,41 @@ public class Inventory {
 
     public void addItem(Item item) {
         // Aquí tu código
+        items.add(item);
     }
 
     public boolean removeItem(String itemName) {
         // Aquí tu código
+        for (var item : items) {
+            if (item.getName().equals(itemName)) {
+                return items.remove(item);
+            }
+        }
         return false;
     }
 
     public Item findItem(String itemName) {
         // Aquí tu código
+
+        for (var item : items) {
+            if (item.getName().equals(itemName)) {
+                return item;
+            }
+        }
+
         return null;
     }
 
+
     public double getTotalWeight() {
         // Aquí tu código
-        return 0.0; // sustituye esto por el valor real
+        // sustituye esto por el valor real
+        double totalSum = 0;
+
+        for (var item : items) {
+            totalSum += item.getWeight() ;
+        }
+        return totalSum;
     }
 
     // Este getter no es necesario modificarlo
